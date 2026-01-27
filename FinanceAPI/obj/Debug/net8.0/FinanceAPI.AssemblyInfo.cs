4735ba911e64898865cb49aba12f552934bc5728
Chris Amaral <chris_lla@hotmail.com>
2026-01-27 17:59:47 -0300

    atualizar versão informacional do assembly para incluir hash de commit

--- FinanceAPI/obj/Debug/net8.0/FinanceAPI.AssemblyInfo.cs
+++ FinanceAPI/obj/Debug/net8.0/FinanceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49abaedfae62b9b1afe983f76884e4476c1c7f81")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

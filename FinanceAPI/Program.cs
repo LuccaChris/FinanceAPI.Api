e50b6dca052dfe48991de0493d4173ba0bc3af49
Chris Amaral <chris_lla@hotmail.com>
2026-01-30 15:25:17 -0300

    fix: corrigir condição para exibir Swagger em ambientes de produção

--- FinanceAPI/Program.cs
+++ FinanceAPI/Program.cs
@@ -88,7 +88,7 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
